a1a0aaa4684083fc414a7a11b89083f33f3229df
zoldiklord <87242582+zoldiklord@users.noreply.github.com>
2022-02-28 12:56:13 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world !");
-        System.out.println("Changes from github !");
+        System.out.println("Changes 2 from github !");
         
     }
 }
